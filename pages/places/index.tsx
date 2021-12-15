e49b2728165f0c3bad1f7a8b25601c20e2226e9b
Yauheni Tsurankou <yauheni.tsurankou@itexus.com>
2021-12-15 08:45:59 +0300

    feat: add map

--- pages/places/index.tsx
+++ pages/places/index.tsx
@@ -1,33 +1,28 @@
 import Head from 'next/head';
 import React from 'react';
-import Row from '../../components/Row';
+import Image from 'next/image';
 
-import { getAllPostsForHome } from '../../lib/api';
-import { Post } from '../../models/post';
-
-export function Places({ allPosts }: { allPosts: Post[] }): React.ReactElement {
+export default function Places(): React.ReactElement {
   return (
     <>
       <Head>
         <title>Uletaem | Места</title>
       </Head>
       <div className="places-page">
-        <h2 className="title">Места</h2>
-        <div className="posts-container grid grid-cols-1 md:grid-cols-3 md:col-gap-16 lg:col-gap-32 gap-10 pt-10 md:gap-16 mb-32">
-          {allPosts.map(post => (
-            <Row isEven post={post} key={post.slug} />
-          ))}
-        </div>
+        <section className="map">
+          <h2 className="title">
+            <span className="title__colored">Весь мир</span> в кармане!
+          </h2>
+          <div className="container">
+            <p className="advantages__description">
+              Выбирайте по городам и странам для быстрого и точного поиска
+            </p>
+            <div className="video">
+              <Image layout="fill" alt="map" src="/images/Frame 10.svg" />
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
 }
-
-export async function getStaticProps() {
-  const allPosts = (await getAllPostsForHome(false)) || [];
-
-  return {
-    props: { allPosts },
-  };
-}
-export default Places;
